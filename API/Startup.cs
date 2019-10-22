3417463d55a46459850abb07a2dcf547fcbee71d
bernoli <yoav.kaplan@gmail.com>
2019-10-22 22:05:00 +0300

    Adding seed data, migrate, auto migration in code.

--- API/Startup.cs
+++ API/Startup.cs
@@ -45,7 +45,7 @@ namespace API
                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,9 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace Persistence {
-    public class DataContext : DbContext {
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
 
         public DbSet<Value> Values { get; set; }
-        public DataContext (DbContextOptions options) : base (options) { }
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>().HasData(
+                new Value{Id=1, Name="Value101"},
+                new Value{Id=2, Name="Value102"},
+                new Value{Id=3, Name="Value103"}
+            );
+        }
     }
 }
