1969c12c98ff6409fe23e6d0869ba4ba8bc14ae9
bernoli <yoav.kaplan@gmail.com>
2019-10-23 08:03:30 +0300

    Inject DataContext into controller (API)

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -5,41 +5,50 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Persistence;
 
-namespace API.Controllers {
-    [Route ("api/[controller]")]
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase {
+    public class ValuesController : ControllerBase
+    {
         private readonly DataContext _dbContext;
 
-        public ValuesController (DataContext dbContext) {
+        public ValuesController(DataContext dbContext)
+        {
             _dbContext = dbContext;
 
         }
 
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get () 
+        public async Task<ActionResult<IEnumerable<string>>> Get()
         {
-            
-            return new string[] { "value1", "value2" };
+
+            var data = await _dbContext.Values.ToAsyncEnumerable().ToArray();
+            return Ok(data);
         }
 
         // GET api/values/5
-        [HttpGet ("{id}")]
-        public ActionResult<string> Get (int id) {
-            return "value";
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            var value = await _dbContext.Values.FindAsync(id);
+            if (value != null)
+                return Ok(value);
+            else
+                return NotFound();
         }
 
         // POST api/values
         [HttpPost]
-        public void Post ([FromBody] string value) { }
+        public void Post([FromBody] string value) { }
 
         // PUT api/values/5
-        [HttpPut ("{id}")]
-        public void Put (int id, [FromBody] string value) { }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) { }
 
         // DELETE api/values/5
-        [HttpDelete ("{id}")]
-        public void Delete (int id) { }
+        [HttpDelete("{id}")]
+        public void Delete(int id) { }
     }
 }
