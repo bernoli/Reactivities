69fd3e8b41cdd04bf19f3379a261de14ba5a4efe
bernoli <yoav.kaplan@gmail.com>
2019-10-31 08:22:43 +0200

    Convert App component to functional and use hooks.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,54 +1,46 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Header, Icon, Container, List, ListItem } from "semantic-ui-react";
 import { IActivity } from "../models/activity";
 
-interface IState {
-  activities: IActivity[];
-}
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
 
-class App extends Component<{}, IState> {
-  readonly state: IState = {
-    activities: []
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get<IActivity[]>("http://localhost:5000/api/activities")
       .then(res => {
-        this.setState({ activities: res.data });
+        setActivities(res.data);
       })
       .catch(err => {
         console.log(err);
       });
-  }
+  }, []);  
+  // note that we must add the empty array [], since useEffect is called anytime the component is rendered.
+  // and we want to make sure it runs only once (in order to mimic the componentDidMount)
+  // see more details https://reactjs.org/docs/hooks-intro.html
+  // If you’re familiar with React class lifecycle methods, you can think of useEffect Hook as:
+  // componentDidMount, componentDidUpdate, and componentWillUnmount combined.
+  // The array is used to control when should the useEffect be called, when it is empty, it means, never (after first time).
 
-  render() {
-    let activitiesElement = null;
-    if (this.state.activities) {
-      activitiesElement = (
+  return (
+    <div>
+      <Container>
+        <Header as="h2">
+          <Icon name="users" />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
         <List>
-          {this.state.activities.map((activity: IActivity) => (
+          {activities.map((activity: IActivity) => (
             <ListItem key={activity.id}>
               {activity.title} - {activity.city} - {activity.venue} - [
               {activity.date}]
             </ListItem>
           ))}
         </List>
-      );
-    }
-    return (
-      <div>
-        <Container>
-          <Header as="h2">
-            <Icon name="users" />
-            <Header.Content>Reactivities</Header.Content>
-          </Header>
-          {activitiesElement}
-        </Container>
-      </div>
-    );
-  }
-}
+      </Container>
+    </div>
+  );
+};
 
 export default App;
